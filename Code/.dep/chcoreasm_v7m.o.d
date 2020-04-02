9503f12f445b04f679f884a3bd38012f01ab32b6
Samuel Bumann <bumann.samuel@gmail.com>
2020-04-02 09:38:32 +0200

    after compile

---
+++ Code/.dep/chcoreasm_v7m.o.d
@@ -0,0 +1,14 @@
+build/obj/chcoreasm_v7m.o: \
+ ../../lib/e-puck2_main-processor/ChibiOS//os/rt/ports/ARMCMx/compilers/GCC/chcoreasm_v7m.s \
+ ../../lib/e-puck2_main-processor/src/chconf.h \
+ ../../lib/e-puck2_main-processor/ChibiOS//os/rt/ports/ARMCMx/chcore.h \
+ ../../lib/e-puck2_main-processor/ChibiOS//os/common/ports/ARMCMx/devices/STM32F4xx/cmparams.h \
+ ../../lib/e-puck2_main-processor/ChibiOS//os/rt/ports/ARMCMx/chcore_v7m.h
+
+../../lib/e-puck2_main-processor/src/chconf.h:
+
+../../lib/e-puck2_main-processor/ChibiOS//os/rt/ports/ARMCMx/chcore.h:
+
+../../lib/e-puck2_main-processor/ChibiOS//os/common/ports/ARMCMx/devices/STM32F4xx/cmparams.h:
+
+../../lib/e-puck2_main-processor/ChibiOS//os/rt/ports/ARMCMx/chcore_v7m.h:
---
+++ Code/.dep/vm.o.d
@@ -0,0 +1,73 @@
+build/obj/vm.o: ../../lib/e-puck2_main-processor/aseba/vm/vm.c \
+ ../../lib/e-puck2_main-processor/aseba/vm/../common/consts.h \
+ ../../lib/e-puck2_main-processor/aseba/vm/../common/types.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\stdlib.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\machine\ieeefp.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\_ansi.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\newlib.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\_newlib_version.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\config.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\features.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\lib\gcc\arm-none-eabi\7.2.1\include\stddef.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\reent.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\_ansi.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\_types.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\machine\_types.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\machine\_default_types.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\lock.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\cdefs.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\machine\stdlib.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\alloca.h \
+ ../../lib/e-puck2_main-processor/aseba/vm/vm.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\string.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\xlocale.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\strings.h \
+ c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\string.h
+
+../../lib/e-puck2_main-processor/aseba/vm/../common/consts.h:
+
+../../lib/e-puck2_main-processor/aseba/vm/../common/types.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\stdlib.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\machine\ieeefp.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\_ansi.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\newlib.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\_newlib_version.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\config.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\features.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\lib\gcc\arm-none-eabi\7.2.1\include\stddef.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\reent.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\_ansi.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\_types.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\machine\_types.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\machine\_default_types.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\lock.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\cdefs.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\machine\stdlib.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\alloca.h:
+
+../../lib/e-puck2_main-processor/aseba/vm/vm.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\string.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\xlocale.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\strings.h:
+
+c:\users\samue\documents\epfl\6.\ semester\microinformatique\microinformatique_tp\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\string.h:
