8a45151171a0ca05fa15ad17f518e0837b6b8f4d
Michael Biselx <michael.biselx@epfl.ch>
2020-04-04 18:05:54 +0200

    same, but minus all the build junk

--- Code/.dep/DataEEPROM.o.d
+++
@@ -1,5 +0,0 @@
-build/obj/DataEEPROM.o: \
- ../../lib/e-puck2_main-processor/src/epuck1x/DataEEPROM.c \
- ../../lib/e-puck2_main-processor/src/epuck1x/DataEEPROM.h
-
-../../lib/e-puck2_main-processor/src/epuck1x/DataEEPROM.h:
