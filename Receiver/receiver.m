598eb24f77f5cb41e175b6a4e3d1ba5c303702f3
Michael Biselx <michael.biselx@epfl.ch>
2020-05-06 10:57:34 +0200

    improvements to matlab receiver routines

--- Receiver/receiver.m
+++ Receiver/receiver.m
@@ -4,6 +4,7 @@ s1 = com_setup("\\\\.\\COM13");    % change this to suit your setup
 
 nb_dom   = 0;
 nb_temp  = 0;
+conversion_factor = 1000/13;%  1000 steps / cm
 
 com_flush(s1);
 
@@ -17,16 +18,16 @@ while (true)
             data_size = uint32(typecast(uint8(com_read(s1, 2)), 'uint16')); % get transmission sizes
             nb_dom    = uint32(typecast(uint8(com_read(s1, 2)), 'uint16'));
             nb_temp   = uint32(typecast(uint8(com_read(s1, 2)), 'uint16'));
-            buffer    = zeros(data_size, 1);
+            buffer    = zeros(1, data_size);
 
             dom_cnt    = 0;                 % arrays start with 1 ...
             temp_cnt   = 0;
             cnt_dn     = 0;
 
-            xd = zeros(nb_dom, 1);
-            yd = zeros(nb_dom, 1);
-            xt = zeros(nb_temp, 1);
-            yt = zeros(nb_temp, 1);
+            xd = zeros(1, nb_dom);
+            yd = zeros(1, nb_dom);
+            xt = zeros(1, nb_temp);
+            yt = zeros(1, nb_temp);
 
             buffer = com_read(s1, data_size);   % get transmission data
 
@@ -45,11 +46,11 @@ while (true)
                 printf(".")
                 xt(temp_cnt+1) = typecast(uint8([buffer(n+1 : n+2)]), 'int16');
                 yt(temp_cnt+1) = typecast(uint8([buffer(n+3 : n+4)]), 'int16');
-                temp_cnt = dom_cnt + 1;
+                temp_cnt = temp_cnt + 1;
                 cnt_dn = 4;
               else
                 disp(" ! transmission error !");
-                printf(char(buffer(n-2:end)));
+                printf(char(buffer(n:end)));
                 com_flush(s1);
                 break;
               end %if
@@ -58,8 +59,11 @@ while (true)
             printf("\ndone\n");
 
             figure(1)
-            plot(xd, yd, 'rd-', xt, yt, 'bx');
+            plot(xd/conversion_factor, yd/conversion_factor, 'rd-;dominant point;', xt/conversion_factor, yt/conversion_factor, 'bx;temporary point;');
             axis("equal");
+            grid on;
+            xlabel("position [cm]");
+            ylabel("position [cm]");
 
             pause(.2);
             com_flush(s1);
